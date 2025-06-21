24b9fcfa8975d18d33f6f9622c4be830367dfcfe
hxbahh <beinghiba01@gmail.com>
2025-06-21 10:57:14 +0530

    Add files via upload

---
+++ Day-6/ModuleExample/main.mjs
@@ -0,0 +1,7 @@
+import {add,sub} from './math.mjs'
+console.log(add(3,4))
+console.log(sub(1,2))
+
+
+import greet from './math.mjs'
+greet('hiba');
---
+++ Day-6/ModuleExample/math.mjs
@@ -0,0 +1,6 @@
+export const add=(a,b)=>a+b
+export const sub=(a,b)=>a-b
+
+export default function greet(name){
+   console.log('Good Morning',name)
+}
